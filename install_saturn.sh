18343e08cb7d917f1b90cc2a349594dc87a82109
Kyoung Whan Choe <choe.kyoung@gmail.com>
2022-08-29 08:48:53 -0700

    Update install_saturn.sh
    fixing shellcheck warnings

--- install_saturn.sh
+++ install_saturn.sh
@@ -3,7 +3,7 @@
 # env variables
 CONDA_DIR=${HOME}/miniconda3
 CONDA_BIN=${CONDA_DIR}/bin
-JUPYTER_SATURN_VERSION=2022.01.06-1
+#JUPYTER_SATURN_VERSION=2022.01.06-1
 
 set -ex
 export PATH="${CONDA_BIN}:${PATH}"
@@ -20,49 +20,49 @@ conda clean -afy
 jupyter lab clean
 jlpm cache clean
 npm cache clean --force
-rm -rf $HOME/.node-gyp
-rm -rf $HOME/.local
+rm -rf "$HOME"/.node-gyp
+rm -rf "$HOME"/.local
 
 
 echo "installing saturn-rapids:"
 mamba env create -f saturn-rapids.yml
 
-${CONDA_DIR}/envs/saturn-rapids/bin/python -m ipykernel install \
+"${CONDA_DIR}"/envs/saturn-rapids/bin/python -m ipykernel install \
         --name saturn-rapids \
         --display-name 'RAPIDS' \
-        --prefix=${CONDA_DIR}
+        --prefix="${CONDA_DIR}"
 
 
 echo "installing saturn-pytorch:"
 mamba env create -f saturn-pytorch.yml
 
-${CONDA_DIR}/envs/saturn-pytorch/bin/python -m ipykernel install \
+"${CONDA_DIR}"/envs/saturn-pytorch/bin/python -m ipykernel install \
         --name saturn-pytorch \
         --display-name 'pytorch' \
-        --prefix=${CONDA_DIR}
+        --prefix="${CONDA_DIR}"
 
 
 echo "installing saturn-tensorflow:"
 mamba env create -f saturn-tensorflow.yml
 
-${CONDA_DIR}/envs/saturn-tensorflow/bin/python -m ipykernel install \
+"${CONDA_DIR}"/envs/saturn-tensorflow/bin/python -m ipykernel install \
         --name saturn-tensorflow \
         --display-name 'tensorflow' \
-        --prefix=${CONDA_DIR}
+        --prefix="${CONDA_DIR}"
 
 
 echo "cleaning conda:"
 
-${CONDA_DIR}/bin/conda clean -afy 
+"${CONDA_DIR}"/bin/conda clean -afy 
 
-find ${CONDA_DIR}/ -name '*.pyc' \( -type f -o -type l \) -delete
-find ${CONDA_DIR}/ -name '*.a' \( -type f -o -type l \) -delete
-find ${CONDA_DIR}/ -name '*.js.map' \( -type f -o -type l \) -delete
+find "${CONDA_DIR}"/ -name '*.pyc' \( -type f -o -type l \) -delete
+find "${CONDA_DIR}"/ -name '*.a' \( -type f -o -type l \) -delete
+find "${CONDA_DIR}"/ -name '*.js.map' \( -type f -o -type l \) -delete
 
 
 echo "creating the workspace:"
 # create workspace and move the examples from the start
-mkdir -p $HOME/workspace
-cd ${HOME}/workspace
-rm -rf $HOME/workspace/saturn-examples
+mkdir -p "$HOME"/workspace
+cd "${HOME}"/workspace
+rm -rf "$HOME"/workspace/saturn-examples
 git clone https://github.com/kywch/saturn-examples.git
